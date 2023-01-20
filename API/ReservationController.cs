93f0433777663b4718dd8e9e4c66c6029314d0c7
ModuguKeerthi <keerthireddymkr143@gmail.com>
2023-01-20 12:25:25 +0530

    FILES

---
+++ API/ReservationController.cs
@@ -0,0 +1,57 @@
+﻿using samplewebApi.Models;
+using samplewebApi.rep;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace samplewebApi.Controllers
+{
+    [EnableCors(origins:"*",headers:"*",methods:"*")]
+    public class ReservationController : ApiController
+    {
+        public readonly InterfaceReserve Ireserve;
+        public ReservationController(Ireserve ireserve)
+        {
+            this.Ireserve = ireserve;
+        }
+        [Route("api/Reservation/GetDetails")]
+        [HttpGet]
+        public IHttpActionResult GetAlldetails()
+        {
+            var GetResults = Ireserve.GetDetails();
+            return Ok(GetResults);
+        }
+        [Route("api/Reservation/insertDetails")]
+        [HttpPost]
+        public IHttpActionResult InsertDetails(ReserveModel res)
+        {
+            var InsertResult = Ireserve.AddDetails(res);
+            return Ok(InsertResult);
+        }
+        [Route("api/Reservation/update")]
+        [HttpPut]
+        public IHttpActionResult UpdateDetails(ReserveModel res)
+        {
+            var EditResult = Ireserve.EditDetails(res);
+            return Ok(EditResult);
+        }
+       [Route("api/Reservation/Delete/{S_No}")]
+       [HttpDelete]
+       public IHttpActionResult DeleteDetails(int S_No)
+       {
+            var DeleteResult = Ireserve.DeleteDetail(S_No);
+            return Ok(DeleteResult);
+       }
+        [Route("api/Reservation/GethotelDetails")]
+        [HttpGet]
+        public IHttpActionResult GetAllHoteldetails()
+        {
+            var GetResults = Ireserve.GethotelDetails();
+            return Ok(GetResults);
+        }
+    }
+}
---
+++ API/ReserveModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace samplewebApi.Models
+{
+    public class ReserveModel
+    {
+        public int S_No { get; set; }
+        public string Hotel { get; set; }
+        public System.DateTime Arrival { get; set; }
+        public System.DateTime Departure { get; set; }
+        public string Type { get; set; }
+        public int Guests { get; set; }
+        public decimal Price { get; set; }
+        public int HotelID { get; set; }
+        public string HotelName { get; set; }
+    }
+}
